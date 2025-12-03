10af2dd271995d1e50ad6dec2d6bad55e1de76b4
Satish  Sampatrao Kore <154883166+SatishKore11@users.noreply.github.com>
2025-12-03 13:27:36 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -86,7 +86,7 @@ SELECT
   sd.sls_due_dt      AS due_date,
   sd.sls_sales       AS sales_amount,
   sd.sls_quantity    AS quantity,
-  sd.sls_price price AS price
+  sd.sls_price       AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
   ON sd.sls_prd_key = pr.product_number
